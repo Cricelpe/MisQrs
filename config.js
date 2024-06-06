d81eda649845c80d4c014b9d8d2638a424ba78b7
Cricelpe <cripg25@gmail.com>
2024-06-06 10:23:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbz_LygfRQ0ybSXS6lQxxjs8XmvAZRDbmG72EsH0SFrunmFG_Qy56BGS5eXrCgcZ3h7M/exec";
